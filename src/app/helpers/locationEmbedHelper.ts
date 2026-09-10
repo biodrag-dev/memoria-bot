b799dcaf27a1896a0f773f350223b7f75e894616
biodegradable-dev <emily.yuluo.wang@gmail.com>
2026-09-10 15:46:53 -0600

    embeds
    .

--- src/app/helpers/locationEmbedHelper.ts
+++ src/app/helpers/locationEmbedHelper.ts
@@ -1,21 +1,46 @@
 import { EmbedBuilder } from "discord.js";
 
-import fs from "fs/promises";
+import fs from "fs";
 import path from "path";
 const jsonsPath = path.resolve(__dirname, "../../../jsons");
 
-var embeds: Record<string, EmbedBuilder> = await loadEmbeds();
+var embeds: Record<string, EmbedBuilder> = loadEmbeds();
 
-async function loadEmbeds() {
-  const data = await fs.readFile(`${jsonsPath}/embeds.json`, "utf8");
+function loadEmbeds() {
+  const data = fs.readFileSync(`${jsonsPath}/embeds.json`, "utf8");
   return JSON.parse(data) as Record<string, EmbedBuilder>;
 }
 
 export function embed(name: string) {
-  loadEmbeds();
-  if (embeds[name]) {
-    return { content: ``, embeds: [embeds[name]] };
-  } else {
-    return { content: `No embed linked to codeword!`, ephemeral: true };
+  return embeds[name] ? {embeds: [embeds[name]]} : {embeds: [new EmbedBuilder().setDescription("embed could not be found!")], ephemeral: true};
+}
+
+export function importEmbeds() {
+  const visual = fs
+    .readFileSync(`${jsonsPath}/locationsVisual.txt`, "utf8")
+    .split("* ");
+  const data = fs
+    .readFileSync(`${jsonsPath}/locationDescs.txt`, "utf8")
+    .split("> ");
+
+  if (visual.length == data.length) {
+    for (var i = 1; i < data.length; i++) {
+      if (data[i]) {
+        const stuff = visual[i]!.split("\n");
+        const embed = new EmbedBuilder();
+        embed.setTitle(stuff[0]?.trim() ?? "FORGOTTEN TITLE");
+        embed.setDescription(data[i]?.trim() ?? "FORGOTTEN DESCRIPTION");
+        embed.setFooter({ text: stuff[1]?.trim() ?? "FORGOTTEN ARTIST" });
+        embed.setImage(stuff[2]?.trim() ?? null);
+        embeds[`location${i}`] = embed;
+      }
+      console.log(i);
+    }
   }
+  console.log(embeds);
+  fs.writeFileSync(
+    `${jsonsPath}/embeds.json`,
+    JSON.stringify(embeds, null, 2),
+    "utf8",
+  );
 }
